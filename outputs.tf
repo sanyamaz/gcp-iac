2e5456d46a5127763a260a370f115f6c318b59ac
Alex Horielov <o_horielov-itssv@kw.com>
2023-01-17 01:05:28 +0200

    fix output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "cloudf_uri" {
-  value = toset([for k, v in google_cloudfunctions2_function.cloud_function : v.service_config])
+  value = toset([for k, v in google_cloudfunctions2_function.cloud_function : v.service_config[*].uri])
 }
