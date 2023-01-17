1e77f16879d92c6b90a8442551c2f6d5ad7828f2
Alex Horielov <o_horielov-itssv@kw.com>
2023-01-17 22:03:54 +0200

    update output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,5 @@
 output "cloudf_uri" {
-  value = toset([for k, v in google_cloudfunctions2_function.cloud_function : v.service_config[*].uri])
+  value = {
+    for k, v in google_cloudfunctions2_function.cloud_function : replace(k, "-", "_") => v.service_config[*].uri
+  }
 }
