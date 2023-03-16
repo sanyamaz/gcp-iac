c5a3f75b8cbac67dec8d7a8ad7063f4eb2419fe1
sanyamaz <sanyamaz@gmail.com>
2023-03-16 14:49:44 +0200

    update tf code

---
+++ modules/service_account/variables.tf
@@ -0,0 +1,9 @@
+variable "sa_name" {}
+
+variable "project" {}
+
+variable "iam_role" {}
+
+variable "sa_type" {
+  default = "serviceAccount"
+}
--- project.tf
+++ project.tf
@@ -4,7 +4,7 @@ resource "google_project" "gcp_project" {
   billing_account = "017434-7C2AB2-62916C"
 }
 
-resource "google_project_service" "app_cloud_services" {
+resource "google_project_service" "this" {
   for_each = var.app_cloud_services
 
   project = var.project
